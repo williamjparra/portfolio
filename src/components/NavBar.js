8644e56aa8b615544db2677c941f6c46cf224eb2
William Parra <w3webservices.net@gmail.com>
2021-03-17 20:42:28 -0400

    se mejoraron algunos estilos dandole retoque all css del footer el home.css y se corrigio un typo en el NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -61,7 +61,7 @@ function Navbar() {
             <div className={ toggleOn === true ? "header-active" : "header-unactive" } >
                 <div className="word-container">
                     <h2>
-                        HI<br />this is my portfolio<br />I hope you like it<br /> i have very intersing projecst down there.<br />
+                        HI<br />this is my portfolio<br />I hope you like it<br /> i have very intersing projects down there.<br />
                         {toggleOn === false  && (
                             <span className="sorry-Words">
                                 Ho sorry if that where to much movement
